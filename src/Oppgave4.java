98551fbc446151a455ad405521c94ef3db369f61
Runar Ingebrigtsen <runar@rin.no>
2011-09-21 11:57:34 +0200

    Recursive tasks solved

--- src/Oppgave4.java
+++ src/Oppgave4.java
@@ -8,14 +8,36 @@ public class Oppgave4 {
 		Scanner keyboard = new Scanner(System.in);
 
 		String input = keyboard.nextLine();
-		String reverse = reverse(input);
 		
-		System.out.println(input + ":" + reverse);
-		System.out.println("The given string is " + (checkPalindrome(input, reverse) ? "a " : "not a " ) + "palindrome.");
+		// Recursive palindrome check
+		System.out.println("The given string is " + (doWePal(input) ? "a " : "not a ") + "palindrome");
 		
+		// Less nice palindrome check
+//		String reverse = reverse(input);
+//		System.out.println(input + " : reverse : " + reverse);
+//		System.out.println("The given string is " + (checkPalindrome(input, reverse) ? "a " : "not a " ) + "palindrome.");
+
 	}
 	
-	public static boolean checkPalindrome(String value, String reverse)
+	// Check for palindrome recursively
+	public static boolean doWePal(String value)
+	{
+		// If the string is one character, then it is a palindrome.
+		// If the string is empty, then we already checked all possible characters.
+		if (value.length() == 0 || value.length() == 1)
+			return true;
+		
+		// If the first and the last character is the same, check the rest of the
+		// string, minus the first and the last character.
+		if (value.charAt(0) == value.charAt(value.length() - 1))
+			return doWePal(value.substring(1, value.length() -1));
+		
+		// Base case.
+		return false;
+	}
+	
+	// Check for palindrome character by character
+	public static boolean manPal(String value, String reverse)
 	{
 		char[] val = value.toCharArray();
 		char[] rev = reverse.toCharArray();
@@ -28,6 +50,7 @@ public class Oppgave4 {
 		return res;
 	}
 	
+	// Return a given string reversed
 	public static String reverse(String value) 
 	{
 		String rev = "";
